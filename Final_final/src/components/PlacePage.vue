26b5931fcd6a4d8fd11d1a98d220c6009dbb0966
Jeff Kim <ek111@duke.edu>
2018-04-29 04:36:40 -0400

    Completed places

--- Final_final/src/components/PlacePage.vue
+++ Final_final/src/components/PlacePage.vue
@@ -5,41 +5,122 @@
         <p id = 'filter'><span class="glyphicon glyphicon-filter"></span> Categories:</p>
       </div>
       <div class='checkbox'>
-        <label class="checkbox-inline"><input type="checkbox" value="">Indoor</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">Outdoor</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">West Campus</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">East Campus</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">Central Campus</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">Food</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">Education</label>
-        <label class="checkbox-inline"><input type="checkbox" value="">Entertainment</label>
+        <label class="checkbox-inline" >
+        <input type="checkbox" value="" v-on:click.stop="addSelected('Indoor')" checked>Indoor
+        </label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click.stop="addSelected('Outdoor')" checked>Outdoor</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('West')" checked>West Campus</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('East')" checked>East Campus</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('Central')" checked>Central Campus</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('Dining')" checked>Dining</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('Education')" checked>Education</label>
+        <label class="checkbox-inline"><input type="checkbox" value="" v-on:click="addSelected('Entertainment')" checked>Entertainment</label>
       </div>
     </div>
-    <div class="row">
-      <div class="col-sm-6 col-md-4" v-for="place in places">
-        <div class="thumbnail">
+    <div class="row" v-show="yes">
+      <div class="col-sm-6 col-md-4" v-for="place in data">
+        <div class="thumbnail" v-if="checkCategory(selected, place)">
           <img v-bind:src="place.imgUrl" v-bind:alt="place.name">
           <div class="caption">
             <h3>{{place.name}}</h3>
             <p><i class="fa fa-map-marker"></i> {{place.address}}</p>
-            <p><a href="#" class="btn btn-danger" role="button">starred</a></p>
-          
+            <p><button type="button" class="btn btn-danger" v-on:click="add_cart(place)">Add to cart</button></p>
           </div>
         </div>
       </div>
     </div>
+    <div id="work"></div>
+        <div id="shoppingCart" v-for="list in cart">
+            {{list.name}}
+            <button type="button" class="btn btn-danger" v-on:click="remove_cart(list), makeLocation()">Remove</button>
+        </div>
+        <button v-on:click="yes=false, makeLocation()">Generate Map</button>
+      <div id="map" v-show="!yes">
+          <gmap-map
+          :center="center"
+          :zoom="zoom"
+          style="width:100%;  height: 500px;"
+        >
+            <gmap-marker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                @click="center=m.position"
+              ></gmap-marker>
+        </gmap-map>
+          
+        </div>
   </div>
+
 </template>
 
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFZB_0uAerF2GJSXCxssPRPgNNDzSH4ow&callback=initMap">
+</script>
 <script>
+import Vue from 'vue'
 var placesJSON = require('../assets/places.json');
 export default {
     name: 'final-forum',
   data() {
     return {
-      places: placesJSON.places
+        data: placesJSON.places,
+        cart: [],
+        yes: true,
+        center: { lat: 36.000666664, lng: -78.93666292 },
+        zoom: 14,
+        markers: [],
+        show: true,
+        selected: ["Indoor", "Outdoor", "West", "East", "Central", "Dining", "Education", "Entertainment"]
     }
-  }
+  },
+    methods: {
+        add_cart(place){
+            var count = 0;
+            for(var i = 0; i<this.cart.length; i++){
+                if(this.cart[i].name === place.name){
+                    count ++;
+                }
+            }
+            if (count == 0){
+                this.cart.push(place);
+            }
+        },
+        remove_cart(place){
+            this.cart.splice(this.cart.indexOf(place), 1);
+        },
+        makeLocation(){
+            for(var i = 0; i < this.cart.length; i++){
+                var location = new google.maps.LatLng(this.cart[i].coordinates[0], this.cart[i].coordinates[1]);
+                this.markers.push({"position": location});
+            }
+        },
+        addSelected(category){
+            var idx = -1;
+            for(var i=0; i<this.selected.length; i++){
+                if(this.selected[i] === category){
+                    idx = i;
+                }
+            }
+            if(idx == -1) {
+                this.selected.push(category);
+            } else {
+                this.selected.splice(idx,1);
+                
+            }
+            console.log(this.selected)
+        },
+        checkCategory(a, b){
+            for (var i = 0; i < a.length; i++){
+                for (var j = 0; j<b.categories.length; j++){
+                    if(a[i] === b.categories[j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
 }
 </script>
 
@@ -47,13 +128,18 @@ export default {
 .places {
     margin: 3%;
 }
-
+    
 .places p {
     color: darkblue;
     border-bottom: solid;
     margin: 2%;
 }
 
+#shoppingCart{
+    background-color: black;
+    width: 30%;
+}
+    
 .checkbox {
     margin: 3%;
 }
--- Final_final/src/main.js
+++ Final_final/src/main.js
@@ -6,6 +6,8 @@ import PlacePage from './components/PlacePage.vue'
 import FinalForum from './components/FinalForum.vue'
 import PostEditor from './components/PostEditor.vue'
 import PostViewer from './components/PostViewer.vue'
+import * as VueGoogleMaps from 'vue2-google-maps';
+
 Vue.use(VueRouter);
 
 const NotFoundComponent = { template: '<div> <h1> Page Not Found </h1> </div>' }
@@ -13,13 +15,20 @@ const NotFoundComponent = { template: '<div> <h1> Page Not Found </h1> </div>' }
 const router = new VueRouter({ 
   mode: 'history',
   routes: [
-    { path: '/final-project-team-35/', component: MainPage },
-    { path: '/final-project-team-35/places', component: PlacePage },
-    { path: '/final-project-team-35/forum', component: FinalForum },
-    { path: '/final-project-team-35/forum/edit_post', name: 'post-editor', component: PostEditor, props: true },
-    { path: '/final-project-team-35/forum/view_post/:postKey', component: PostViewer },
+    { path: '/', component: MainPage },
+    { path: '/places', component: PlacePage },
+    { path: '/forum', component: FinalForum },
+    { path: '/forum/edit_post', name: 'post-editor', component: PostEditor, props: true },
+    { path: '/forum/view_post/:postKey', component: PostViewer },
     { path: '*', component: MainPage }
   ]
+});
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyBFZB_0uAerF2GJSXCxssPRPgNNDzSH4ow',
+        libraries: "places"
+    }
 });
 
 new Vue({
