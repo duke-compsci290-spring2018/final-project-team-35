e4c7310de120f40cdf1ecfe36f6c05274cf02d98
Amy Kim <yeonwoo@gim-yeon-us-MacBook-Pro.local>
2018-04-30 03:45:10 -0400

    rebuild dist

--- src/main.js
+++ src/main.js
@@ -10,20 +10,14 @@ import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.use(VueRouter);
 
-const NotFoundComponent = { template: '<div> <h1> Page Not Found </h1> </div>' }
-
-// FOR production
-const rootDir = '/final-project-team-35'
-//const rootDir = ''
-
 const router = new VueRouter({ 
   mode: 'history',
   routes: [
-    { path: rootDir+'/', component: MainPage },
-    { path: rootDir+'/places', component: PlacePage },
-    { path: rootDir+'/forum', component: FinalForum },
-    { path: rootDir+'/forum/edit_post', name: 'post-editor', component: PostEditor, props: true },
-    { path: rootDir+'/forum/view_post/:postKey', component: PostViewer },
+    { path: '/final-project-team-35/', component: MainPage },
+    { path: '/final-project-team-35/places', component: PlacePage },
+    { path: '/final-project-team-35/forum', component: FinalForum },
+    { path: '/final-project-team-35/forum/edit_post', name: 'post-editor', component: PostEditor, props: true },
+    { path: '/final-project-team-35/forum/view_post/:postKey', component: PostViewer },
     { path: '*', component: MainPage }
   ]
 });
